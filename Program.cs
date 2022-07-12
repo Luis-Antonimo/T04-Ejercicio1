1156c290aaf8915cb942ebedbb967ab85c9486c4
Luis Antónimo <luisantonimo@gmail.com>
2022-07-12 08:28:19 +0200

    ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace T04Ejercicio1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 10;
+            int b = 20;
+            int c = 30;
+            int d = a + b + b;
+            Console.WriteLine("Las variables originales son A:{0} B:{1} C:{2}", a, b, c);
+            Console.WriteLine("La suma de las variables es: " + d);
+        }
+    }
+}
